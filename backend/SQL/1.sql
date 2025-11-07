f3fd65874805a217630fd2ebd78aca82b4e3e593
TAYNANBRINGHENTI <taynanbringhenti14@gmail.com>
2025-11-07 16:20:40 -0300

    Riquelme não gostou, mestre mandou apagar, sofro racismo nesse grupo

--- backend/SQL/1.sql
+++
@@ -1,4 +0,0 @@
-SELECT datahora, horaleitura, temperatura
-FROM leituraptqa
-WHERE datahora BETWEEN '2025-06-01' AND '2025-06-10'
-ORDER BY datahora ASC;
--- backend/SQL/5.sql
+++
@@ -1,4 +0,0 @@
-SELECT *
-FROM leituraptqa
-WHERE pressao < 1000
-  AND datahora BETWEEN '2025-06-01' AND '2025-06-10';
